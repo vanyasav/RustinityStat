afd4867852156a0c21ffc07373c0563d4aef56d9
vanyasav <ivan.savilov@gmail.com>
2023-12-29 01:52:38 +0100

    Auto-generated commit [ci skip]

--- src/app/pipes/seconds-to-hours.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import {SecondsToHoursPipe} from './seconds-to-hours.pipe';
-
-describe('SecondsToHoursPipe', () => {
-  it('create an instance', () => {
-    const pipe = new SecondsToHoursPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
